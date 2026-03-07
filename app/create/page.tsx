746a87ae0bfab3a67060faa4e99ce1f88ae6312c
Faizan Abid <Faizanabid36@gmail.com>
2026-03-07 03:29:17 -0400

    Feature: Added Toaster

---
+++ app/create/page.tsx
@@ -0,0 +1,13 @@
+import EventCreationForm from "@/components/EventCreationForm";
+
+const Create = () => {
+  return (
+    <section className="py-5">
+      <div className="container mx-auto">
+        <EventCreationForm />
+      </div>
+    </section>
+  );
+};
+
+export default Create;
